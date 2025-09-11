8d6dcaf7f29ecd580ca50a034998ff153bfd464a
George Nagy II <george.nagy0969@gmail.com>
2025-09-11 07:54:17 -0500

    added feedFetch func

--- main.go
+++ main.go
@@ -2,8 +2,51 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func fetchFeed(rssURL string) []byte {
+	res, err := http.Get(rssURL)
+	check(err)
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		fmt.Printf("Response failed with status code: %d\n", res.StatusCode)
+		os.Exit(1)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	check(err)
+
+	return body
+}
+
+/*
+func writeFeed(stringFeed string) {
+	file, err := os.Create("/tmp/dat1")
+	check(err)
+
+	defer file.Close()
+
+	n3, err := file.WriteString(stringFeed)
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n3)
+
+	file.Sync()
+}
+
+
+func parseFeed(xmlContent string) {
+}
+*/
 
 func main() {
 	if len(os.Args) < 2 {
@@ -13,4 +56,6 @@ func main() {
 
 	rssFeed := os.Args[1]
 	fmt.Printf("Looking up activity for: %s\n\n", rssFeed)
+
+	fmt.Printf("%s", fetchFeed(rssFeed))
 }
