778063d94bcd39a9e8e08fe2a39c7bcd0aea3302
George Nagy II <george.nagy0969@gmail.com>
2025-09-12 04:02:33 -0500

    added timeChecker function

--- main.go
+++ main.go
@@ -45,9 +45,10 @@ func fetchFeed(rssURL string) []byte {
 	return body
 }
 
-func parseFeed(xmlContent []byte) {
+func parseFeed(xmlContent []byte) error {
 	var ourFeed RSS
-	xml.Unmarshal(xmlContent, &ourFeed)
+	err := xml.Unmarshal(xmlContent, &ourFeed)
+	check(err)
 
 	fmt.Printf("%s\n\n", ourFeed.Channel.Title)
 
@@ -55,11 +56,39 @@ func parseFeed(xmlContent []byte) {
 		fmt.Println("------------------")
 		fmt.Printf("%s\n\n", v.Title)
 		fmt.Printf("%s\n", v.Link)
-		parsedTime, err := time.Parse(time.RFC1123Z, v.PubDate)
-		check(err)
+
+		parsedTime := timeChecker(v.PubDate)
 		formattedTime := parsedTime.Format(time.DateTime)
 		fmt.Printf("%s\n", formattedTime)
 	}
+	return nil
+}
+
+func timeChecker(ourTime string) time.Time {
+	formats := []string{
+		time.RFC1123Z,
+		time.RFC1123,
+		time.RFC822Z,
+		time.RFC822,
+		time.RFC3339Nano,
+		time.RFC3339,
+		time.RFC850,
+		time.ANSIC,
+		time.UnixDate,
+		time.RubyDate,
+		time.StampMilli,
+		time.StampMicro,
+		time.StampNano,
+		time.DateTime,
+		time.DateOnly,
+	}
+	var goodTime time.Time
+	for _, v := range formats {
+		if t, err := time.Parse(v, ourTime); err == nil {
+			goodTime = t
+		}
+	}
+	return goodTime
 }
 
 func main() {
